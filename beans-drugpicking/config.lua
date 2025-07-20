150fedbac6243fd5798aba0ea81826c0ca2240cf
MacaroniandBeans <macaroniandbeansgaming@gmail.com>
2025-07-20 07:46:38 -0500

    Update config.lua

--- beans-drugpicking/config.lua
+++ beans-drugpicking/config.lua
@@ -1,11 +1,8 @@
 Config = {}
 
-Config.Locale = 'en'
-
-Config.LicenseEnable = false -- enable processing licenses? The player will be required to buy a license in order to process drugs. 
-
 Config.SharedPickupXPType = 'drug'
 
+-- Tiered reward amounts based on level
 Config.SharedPickupTiers = {
     [1] = 1,    -- level 1+
     [5] = 2,    -- level 5+
@@ -19,22 +16,31 @@ Config.XPCategory = "drug" -- Change to match your XP category name in pickle_xp
 
 Config.CircleZones = {
     WeedField = {
+        enabled = true,
         coords = vector3(2031.44, 4878.57, 42.89),
         radius = 15.0,
         item = 'water_bottle',
-        label = 'Cannabis'
+        label = 'Cannabis',
+        model = `prop_weed_02`,
+        icon = 'fas fa-leaf'
     },
     ChemicalField = {
+        enabled = true,
         coords = vector3(1547.21, 3817.65, 30.54),
         radius = 15.0,
         item = 'water_bottle',
-        label = 'Chemicals'
+        label = 'Chemicals',
+        model = `prop_barrel_01a`,
+        icon = 'fas fa-flask'
     },
     CocaField = {
+        enabled = true,
         coords = vector3(-2125.3, 1427.3, 284.79),
         radius = 15.0,
         item = 'water_bottle',
-        label = 'Coca Leaf'
+        label = 'Coca Leaf',
+        model = `prop_plant_01a`,
+        icon = 'fas fa-leaf'
     }
 }
 
